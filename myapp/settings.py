3aea8158eda9003d9ac600bfc502890aae7e6f84
Harish140303 <harishdestro1810@gmail.com>
2026-03-30 14:54:24 +0530

    add postgresql database

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from dotenv import load_dotenv
 load_dotenv()  
@@ -87,11 +88,18 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
